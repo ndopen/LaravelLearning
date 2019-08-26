65410dea64908f74fcac059abafaf38c73760371
cockroach <yyx198907@gmail.com>
2019-08-26 16:29:32 +0800

    完成个人编辑页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
     public function show(User $user){
         // dd(compact('user'));
         return view('users.show', compact('user'));
+    }
+
+    // users edit
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // update
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功!');
     }
 }
