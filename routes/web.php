8a0af4ed12b7c5c82aec0a5e388ce50e57f53d38
cockroach <yyx198907@gmail.com>
2019-08-28 20:32:33 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 // UsersController
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+// 帖子话题资源路由
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
